c8d07aa065527a924dae83255197ff0574bb7271
Rao Nakka <nageswara.rao.nakka@accenture.com>
2018-12-06 10:00:40 +0530

    commneted the test code

--- src/test/java/org/ravi/helloworld/HelloTest.java
+++ src/test/java/org/ravi/helloworld/HelloTest.java
@@ -15,19 +15,19 @@ import org.testng.annotations.AfterTest;
 
 public class HelloTest {
 
-	WebDriver driver;
+	/*WebDriver driver;
 	WebElement text;
 	@BeforeTest
 	public void beforeTest() {
-		/*System.setProperty("webdriver.gecko.driver", "resources\\geckodriver32.exe");
-		driver = new FirefoxDriver();*/
+		System.setProperty("webdriver.gecko.driver", "resources\\geckodriver32.exe");
+		driver = new FirefoxDriver();
 		System.setProperty("webdriver.chrome.driver","resources\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get("http://localhost:8080/Helloworld.war/");
 		driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);
 	}
 
-	/*@Test
+	@Test
 	public void f() {
 
 		text = driver.findElement(By.xpath("html/body/h2"));
