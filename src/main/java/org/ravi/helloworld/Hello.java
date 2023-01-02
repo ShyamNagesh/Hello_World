38ae8c28760b866b930da25fc65e01b4f961e32e
ShyamNagesh <shyamnagesh@gmail.com>
2023-01-02 07:40:59 +0530

    Updated Hello.java file.

--- src/main/java/org/ravi/helloworld/Hello.java
+++ src/main/java/org/ravi/helloworld/Hello.java
@@ -1,10 +1,16 @@
-package org.ravi.helloworld;
+package org.ravi.helloworld.Hello;
 
 public class Hello {
-	
-	public String displayHelloMessage(String message){
-		message = "hi";
-		return message;
-	}
 
+    private static final String MESSAGE = "Hello World!";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println(MESSAGE);
+    }
+
+    public String getMessage() {
+        return MESSAGE;
+    }
 }
